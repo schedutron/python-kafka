1cca7adf48792b7f480c74a351923356c372bbb8
Saurabh S. Chaturvedi <saurabh.chaturvedi63@gmail.com>
2018-04-20 00:49:43 +0530

    Done with producer

--- producer.py
+++ producer.py
@@ -10,3 +10,29 @@ producer = SimpleProducer(kafka)
 
 # Assign a topic
 topic = 'Python'
+
+def video_emitter(video):
+    # Open the video
+    video = cv2.VideoCapture(video)
+    print(' emitting.....')
+
+
+    # read the file
+    while(video.isOpened):
+        # read the image in each frame
+        success, image = video.read()
+        # check if the file has read to the end
+        if not success:
+            break
+        # convert the image png
+        ret, jpeg = cv2.imencode('.png', image)
+        # Convert the image to bytes and send to kafka
+        producer.send_messages(topic, jpeg.tobytes())
+        # To reduce CPU usage create sleep time of 0.2 sec
+        time.sleep(0.2)
+    # clear the capture
+    video.release()
+    print('done emitting')
+
+if __name__ == '__main__':
+    video_emitter(' Concrete\ Problems\ in\ AI\ Safety\ \(Paper\)\ -\ Computerphile.mp4')
